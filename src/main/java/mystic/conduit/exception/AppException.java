3d55a7a7f9f20e173eb95edffd92232486362032
MyStic <khanh.votuan@hcmut.edu.vn>
2024-07-29 22:07:22 +0700

    feat: handle error exception

---
+++ src/main/java/mystic/conduit/exception/AppException.java
@@ -0,0 +1,10 @@
+package mystic.conduit.exception;
+
+import lombok.Getter;
+
+@Getter
+public class AppException extends RuntimeException{
+    public AppException(Error error) {
+        super(error.getMessage());
+   }
+}
---
+++ src/main/java/mystic/conduit/exception/ErrorMessages.java
@@ -0,0 +1,30 @@
+package mystic.conduit.exception;
+
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@JsonTypeName("errors")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
+public class ErrorMessages {
+    private final List<Map<String, String>> body;
+
+    public ErrorMessages() {
+        body = new ArrayList<Map<String, String>>();
+    }
+
+
+    public void append(HashMap<String, String> message) {
+        body.add(message);
+    }
+
+
+
+}
