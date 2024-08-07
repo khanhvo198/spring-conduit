8355f952e5758d6f3c4012afb7af9c0919f8a619
MyStic <khanh.votuan@hcmut.edu.vn>
2024-08-07 23:17:07 +0700

    fix: update user

--- src/main/java/mystic/conduit/domain/user/dto/UpdateDto.java
+++ src/main/java/mystic/conduit/domain/user/dto/UpdateDto.java
@@ -1,11 +1,15 @@
 package mystic.conduit.domain.user.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
+@JsonTypeName("user")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
 public class UpdateDto {
     private String username;
     private String email;
